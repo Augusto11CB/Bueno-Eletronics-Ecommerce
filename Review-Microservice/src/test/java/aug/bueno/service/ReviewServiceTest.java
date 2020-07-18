8fbf10f446c8c593e3871dfdda8b5474ca533ba9
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-07-18 14:44:35 -0300

    Added first unittest for reviewServiceTest

---
+++ Review-Microservice/src/test/java/aug/bueno/service/ReviewServiceTest.java
@@ -0,0 +1,44 @@
+package aug.bueno.service;
+
+import aug.bueno.domain.Review;
+import aug.bueno.domain.ReviewEntry;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Date;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ReviewServiceTest {
+
+    @Autowired
+    private ReviewService service;
+
+    @MockBean
+    private ReviewService repository;
+
+    @Test
+    void testFindByIdSuccess(){
+        Review mockReview = new Review("reviewId", 1L, 1);
+
+        Date now = new Date();
+
+        mockReview.getEntries().add(new ReviewEntry("test-user", now, "Nice"));
+        doReturn(Optional.of(mockReview)).when(repository).findById(mockReview.getId());
+
+        Optional<Review> reviewReturned = service.findById(mockReview.getId());
+
+        assertTrue(reviewReturned.isPresent());
+        assertSame(reviewReturned.get(), mockReview);
+    }
+
+}
